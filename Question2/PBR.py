52b5636493e039a23b4a44ffab2fccc3aa9f01f4
hlgreer <hannahgreer@rogers.com>
2017-02-11 13:54:22 -0500

    Renaming of File dpll to pbr, some comments made

--- Question2/PBR.py
+++ Question2/PBR.py
@@ -4,3 +4,24 @@ For CISC 352, Winter 2017
 
 Assignment 2 - Proof by Refutation
 '''
+
+import re
+
+def loadTextfile(textfile):
+    with open(textfile) as f:
+        lines = f.read().splitlines()
+    return lines
+
+
+'''
+# We may need to read in from the stdin input?
+def readInput():
+    # until the therefore is read, just grab each item and send to CNF
+    end = False
+    while end == False:
+        theInput = raw_input(">")
+        match = re.match(r'therefore(.*)', theInput.lower())
+        if match: # If the input contains the Therefore statement
+            # stop taking input
+'''
+
