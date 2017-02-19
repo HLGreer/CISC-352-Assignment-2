636e9c53018087a3f1c8ebc856fc4516ba9e84ab
hillary <hillary.elrick@gmail.com>
2017-02-19 01:07:14 -0500

    Integrated Q1 in Q2, imports CNF function from other folder (should be tested on another computer as well). The convertToClause method copied from Q1, called convertToClause_PBR and modified slightly for integration. Additionally, PBR will not import txt files with name out.txt as this is the name of its output file & can cause confusion when running function multiple times. Bug found where the results do not stay constant when letters replaced by words (i.e.) A replaced by Ant, C replaced with Cat in test.txt does not behave as expected. Will investigate in morning

--- Question2/PBR.py
+++ Question2/PBR.py
@@ -14,7 +14,7 @@ def loadTextfile(textfile):
         line = line.lower()
     return lines
 
-# Convert the string returned from the CNF function to the
+# Convert the string returned from the CNF function to list
 def cnfToList(cnfStr):
     noSquigMatch = re.match(r'\{(.*)\}', cnfStr)
     noSquig = noSquigMatch.group(1)
@@ -23,14 +23,19 @@ def cnfToList(cnfStr):
         splitArr[i] = splitArr[i].split(',')
     return splitArr
 
-#coded by other group
-#notes to other group:
-# return as lowercase plz
-# no whitespace
-# individual atoms in brackets
-# i.e.) {(a,b),(c,!d),(a)}
+#import function from Question 1
 def CNF(predicate):
-    return
+    import sys, os
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'Question1'))
+    from convertToCNF import convertToCNF
+    output = convertToCNF(predicate)
+    #ensures atoms are put in parentheses
+    if len(output) > 4:
+        return '{' + convertToClause_PBR('(' + output + ')') + '}'
+    elif len(output) == 4:
+        return '{' + output + '}'
+    return '{' + '(' + output + ')' + '}'
+
 
 def negateL(l):
     if len(l) > 1:
@@ -46,7 +51,6 @@ def unit_propagate(F):
         for i in range(0,len(F)):
             if len(F[i]) == 1:
                 l = F[i][0]
-                print l
                 break
         if l == "":
             hasUnit = False
@@ -59,7 +63,7 @@ def remove_unit(F,l):
     negL = negateL(l)
     for clause in F:
         if l not in clause and negL not in clause:
-            outF = outF.append(clause)
+            outF.append(clause)
         elif negL in clause:
             clause[:] = [x for x in clause if x != negL]
             outF.append(clause)
@@ -77,8 +81,59 @@ def dpll(F):
     FsubNL = remove_unit(F,negL)
     return dpll(FsubL) or dpll(FsubNL)
 
+#convert to clause modified from Question 1
+def convertToClause_PBR(cnf):
+    # flag
+    opened = False
+    # initial {
+    output = ""
+    # loop through the expression
+    for i in range(len(cnf)):
+        # if it isnt v or ^, check for brackets or add atom
+        if cnf[i] != "v" and cnf[i] != "^":
+            # if it is an open bracket, check if we should include it
+            if cnf[i] == "(":
+                # check if we already opened a bracket or if the next element is a bracket
+                if opened or cnf[i + 1] == "(":
+                    # skip this one, we dont want 2
+                    pass
+                # Checks if the expression is in the form: (A), skips bracket
+                else:
+                    #loop through from i to end, checking if there are any other expressions in the ()
+                    for j in range(i,len(cnf)):
+                        #if there is another expression we can add brackets
+                        if cnf[j] == "v" and opened == False:
+                            output += "("
+                            opened = True
+                        #no other expressions, remove brackets
+                        elif cnf[j] == ")":
+                            pass
+            # Check for closed brackets
+            elif cnf[i] == ")":
+                # If we opened a bracket and we are at the end, or at an ^ add
+                # it
+                if opened and (i == len(cnf) - 1 or cnf[i + 1] == "^"):
+                    output += ")"
+                # otherwise we dont want it
+                else:
+                    pass
+            # Adds the atoms
+            else:
+                output += cnf[i]
+        # it is v make it a comma but dont change flags as we are inside the
+        # brackets
+        elif cnf[i] == "v":
+            output += ","
+        # it is an ^, we can add a comma and reset brackets
+        else:
+            output += ","
+            opened = False
+    return output
+
 def main():
-    txtFiles = glob.glob('./*.txt')
+    txtFiles = glob.glob('./*.txt') #imports any file in directory with .txt extension
+    if './out.txt' in txtFiles:
+        txtFiles.remove('./out.txt') #removes if file called out.txt
     fileIn = txtFiles[0]
     predicates = loadTextfile(fileIn)
     finalCNF = []
@@ -87,8 +142,8 @@ def main():
         conclusion = concRE.match(predicates[i]) #check if matches regex for conclusion.
         if conclusion:
             predicates[i] = "!(" + conclusion.group(1) + ")" #Negate the conclusion
-        finalCNF += CNF(predicates[i])
-    sat = dpll(finalCNF)
+        finalCNF += cnfToList(CNF(predicates[i]))
+    sat = dpll(finalCNF) #runs the dpll algorithm
     outFile = open("out.txt","w")
     if sat:
         outFile.write("The conclusion does not follow logically from the premises.")
@@ -96,7 +151,6 @@ def main():
         outFile.write("The conclusion follows logically from the premises.")
     outFile.close()
     print "Result stored in out.txt"
-
 
 if __name__ == "__main__":
     main()
